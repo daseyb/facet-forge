3760349af8128d0ca0ba2adf5aa1d117cd3d2538
Dario Seyb <dario.seyb@gmail.com>
2023-08-07 20:39:58 -0400

    fixed up ggx

--- include/bsdfs/NDFs/GGX.h
+++ include/bsdfs/NDFs/GGX.h
@@ -31,74 +31,67 @@ public:
     }
 
     // distribution of slopes
-    virtual double P22(const double slope_x, const double slope_y) const;
-    // cross section
-    virtual double sigma(const Vector3 &wi) const;
-    // sample the distribution of visible slopes with roughness=1.0
-    virtual Vector2 sampleP22_11(const double theta_i) const;
-};
-
-double GGXNDF::P22(const double slope_x, const double slope_y) const
-{
-    const double tmp = 1.0 + slope_x * slope_x / (m_roughness_x * m_roughness_x) + slope_y * slope_y / (m_roughness_y * m_roughness_y);
-    return 1.0 / (M_PI * m_roughness_x * m_roughness_y) / (tmp * tmp);
-}
-
-Vector2 GGXNDF::sampleP22_11(const double theta_i) const
-{
-    Vector2 slope;
-
-    const double U = RandomReal();
-    const double U_2 = RandomReal();
-
-    if (theta_i < 0.0001)
-    {
-        const double r = sqrt(U / (1 - U));
-        const double phi = 2 * Pi * U_2;
-        slope.x = r * cos(phi);
-        slope.y = r * sin(phi);
-        return slope;
+    virtual double P22(const double slope_x, const double slope_y) const {
+        const double tmp = 1.0 + slope_x * slope_x / (m_roughness_x * m_roughness_x) + slope_y * slope_y / (m_roughness_y * m_roughness_y);
+        return 1.0 / (M_PI * m_roughness_x * m_roughness_y) / (tmp * tmp);
     }
 
-    // constant
-    const double sin_theta_i = sin(theta_i);
-    const double cos_theta_i = cos(theta_i);
-    const double tan_theta_i = sin_theta_i / cos_theta_i;
+    // cross section
+    virtual double sigma(const Vector3 &wi) const {
+        if (wi.z > 0.9999)
+            return 1.0;
+        if (wi.z < -0.9999)
+            return 0.0;
 
-    // slope associated to theta_i
-    const double slope_i = cos_theta_i / sin_theta_i;
+        const double theta_i = acos(wi.z);
+        const double sin_theta_i = sin(theta_i);
+        const double roughnessi = roughness_i(wi);
 
-    // projected area
-    const double sigma = 0.5 * (cos_theta_i + 1);
-    if (sigma < 0.0001f || sigma != sigma)
-        return Vector2(0, 0);
+        return 0.5 * (wi.z + sqrt(wi.z * wi.z + sin_theta_i * sin_theta_i * roughnessi * roughnessi));
+    }
 
-    // normalization coefficient
-    const double c = 1.0 / sigma;
+    // sample the distribution of visible slopes with roughness=1.0
+    virtual Vector2 sampleP22_11(const double theta_i) const{
+        Vector2 slope;
 
-    const double A = 2 * U / cos_theta_i / c - 1;
-    const double B = tan_theta_i;
-    const double tmp = 1 / (A * A - 1);
+        const double U = RandomReal();
+        const double U_2 = RandomReal();
 
-    const double D = sqrt(std::max(0.0, B * B * tmp * tmp - (A * A - B * B) * tmp));
-    const double slope_x_1 = B * tmp - D;
-    const double slope_x_2 = B * tmp + D;
-    slope.x = (A < 0.0 || slope_x_2 > 1.0 / tan_theta_i) ? slope_x_1 : slope_x_2;
-    slope.y = sqrt(-1 - slope.x * slope.x + (1 + slope.x * slope.x) / pow(1 - U_2, 2.0 / 3.0)) * sin(2 * Pi * RandomReal());
+        if (theta_i < 0.0001)
+        {
+            const double r = sqrt(U / (1 - U));
+            const double phi = 2 * Pi * U_2;
+            slope.x = r * cos(phi);
+            slope.y = r * sin(phi);
+            return slope;
+        }
 
-    return slope;
-}
+        // constant
+        const double sin_theta_i = sin(theta_i);
+        const double cos_theta_i = cos(theta_i);
+        const double tan_theta_i = sin_theta_i / cos_theta_i;
 
-double GGXNDF::sigma(const Vector3 &wi) const
-{
-    if (wi.z > 0.9999)
-        return 1.0;
-    if (wi.z < -0.9999)
-        return 0.0;
+        // slope associated to theta_i
+        const double slope_i = cos_theta_i / sin_theta_i;
 
-    const double theta_i = acos(wi.z);
-    const double sin_theta_i = sin(theta_i);
-    const double roughnessi = roughness_i(wi);
+        // projected area
+        const double sigma = 0.5 * (cos_theta_i + 1);
+        if (sigma < 0.0001f || sigma != sigma)
+            return Vector2(0, 0);
 
-    return 0.5 * (wi.z + sqrt(wi.z * wi.z + sin_theta_i * sin_theta_i * roughnessi * roughnessi));
-}
+        // normalization coefficient
+        const double c = 1.0 / sigma;
+
+        const double A = 2 * U / cos_theta_i / c - 1;
+        const double B = tan_theta_i;
+        const double tmp = 1 / (A * A - 1);
+
+        const double D = sqrt(std::max(0.0, B * B * tmp * tmp - (A * A - B * B) * tmp));
+        const double slope_x_1 = B * tmp - D;
+        const double slope_x_2 = B * tmp + D;
+        slope.x = (A < 0.0 || slope_x_2 > 1.0 / tan_theta_i) ? slope_x_1 : slope_x_2;
+        slope.y = sqrt(-1 - slope.x * slope.x + (1 + slope.x * slope.x) / pow(1 - U_2, 2.0 / 3.0)) * sin(2 * Pi * RandomReal());
+
+        return slope;
+    }
+};
